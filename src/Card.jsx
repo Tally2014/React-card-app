63f47fb880c135686cfe0ff6a75a84ac95030a83
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-06 22:15:11 +0200

    Include image for the card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,11 @@
+import profilePic from './assets/naruto crying.webp'
+
 export default function Card() {
   return (
     <div className="card">
-        <img src="" alt="Profile Picture" />
-        <h1>Coded Solutions</h1>
-        <p>Innovative web development startup revolutionizing online experiences through cutting-edge design and seamless functionality.</p>
+        <img src={profilePic} alt="Profile Picture" />
+        <h1>Emotional Naruto</h1>
+        <p>Flashbacks reveal Naruto's painful orphaned and outcast childhood.</p>
     </div>
   )
 }
