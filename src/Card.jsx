6fe0b5dc69eda5c72dc519d4b33f3a6e8114ff97
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-06 22:33:54 +0200

    Style .card-title className

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
   return (
     <div className="card">
         <img className="card-image" src={profilePic} alt="Profile Picture" />
-        <h1>Emotional Naruto</h1>
+        <h2 className="card-title">Emotional Naruto</h2>
         <p>Flashbacks reveal Naruto's painful orphaned and outcast childhood.</p>
     </div>
   )
